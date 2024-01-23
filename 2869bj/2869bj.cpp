00b8bc5322507ae97015799d8011eee956da9f48
Suri <quffl2002@naver.com>
2024-01-24 03:49:11 +0900

    2869 브론즈 1

---
+++ 2869bj/2869bj.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,v;
+    int day = 0;
+
+    cin >> a >> b >> v;
+
+    day = ((v - b) % (a - b));
+    if (day==0) {
+        cout << ((v - b) / (a - b));
+    }
+    else {
+        cout << (((v - b) / (a - b)) + 1);
+    }
+    
+}
